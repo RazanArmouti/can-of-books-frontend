186e497187e96597b0c2f7795f4146d6c544acbb
Razan Armouti <razanarmote@yahoo.com>
2021-09-21 02:00:17 +0300

    lab12

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import BestBooks from './BestBooks';
 import Nav from './components/NAV';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import BookFormModal from './components/BookFormModal';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 import {
   BrowserRouter as Router,
@@ -13,12 +16,63 @@ import {
 } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booksData: [],
+      owner: {
+        ownerName: "Razan Armouti",
+        email: "admin@gmail.com"
+      },
+      title: '',
+      description: '',
+      image: '',
+      showModal: false
+
+    }
+  }
+  componentDidMount = async () => {
+    let data = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/books`);
+    this.setState({
+      booksData: data.data
+    })
+    console.log(this.state.booksData);
+  }
+  handleBookInput = () => {
+    this.setState({
+      showModal: true
+
+    })
+    console.log(this.state.showModal)
+
+  }
+  handleClose = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+  handleOpen = (image, title, description) => {
+    this.setState({
+      showModal: true,
+      image: image,
+      title: title,
+      description: description
+
+    })
+  }
+
+  deleteBook = async (bookID) => {
+    // let catsInfo = await axios.delete(`${process.env.REACT_APP_SERVER}/deleteCat?catID=${catID}`)
+    let books = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/books/${bookID}?ownerName=${this.state.owner.email}`);
+    this.setState({
+      booksData: books.data
+    })
+
+  }
 
   render() {
     return (
       <div>
-        <BestBooks />
-        <br />
         <Router>
           <Nav />
           <Switch>
@@ -28,10 +82,26 @@ class App extends Component {
             <Route path="/Profile">
               <Profile />
             </Route>
-
           </Switch>
         </Router>
-      </div>
+        <br />
+
+        <BestBooks booksData={this.state.booksData} />
+
+        <br />
+        <Button variant="primary" onClick={this.handleBookInput} >Add New Book</Button>
+
+
+        {
+
+          this.state.showModal ? <BookFormModal showModal={this.state.showModal} handleOpen={this.handleOpen} handleClose={this.handleClose} /> : ''
+
+        }
+
+        <br />
+        <Button variant="danger" onClick={this.deleteBook}>Delete Book</Button>
+
+      </div >
     )
   }
 }
---
+++ src/components/BookFormModal.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import { Modal, Button } from 'react-bootstrap';
+import axios from 'axios';
+require('dotenv').config();
+ class BookFormModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: '',
+            title: '',
+            description: ''
+        }
+        console.log( this.props.showModal)
+    }
+    getHandleOpen = () => {
+       // this.handleNewBook
+       console.log(this.state.image, this.state.title, this.state.description);
+        this.props.handleOpen(this.state.image, this.state.title, this.state.description)       
+        let config={
+          method:"POST",
+          baseURL:`${process.env.REACT_APP_BACKEND_URL}`,
+          url:"/create-book",
+          data:{
+            image:this.state.image,
+            title:this.state.title,
+            description:this.state.description
+          }
+        }
+        axios(config).then(response=>{
+            this.setState({
+                booksData: response.data
+            })
+          })
+          console.log(this.state.booksData);
+    }
+    handleNewtitle = e => {   
+        console.log(e.target.value)     
+        this.setState({           
+            title: e.target.value,
+      
+        });
+
+        console.log(this.state.title)
+    }
+    handleNewdesc = e => {        
+        console.log(e.target.value)  
+        this.setState({           
+            description: e.target.value
+        });
+        console.log(this.state.description)
+    }
+    handleNewimg = e => {
+        console.log(e.target.value)  
+        this.setState({
+            image: e.target.value,
+           
+        });
+        console.log(this.state.image)
+    }
+
+    render() {
+        return (
+
+            <Modal show={this.props.showModal} onHide={this.props.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title><h1>Add New Book</h1></Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <form >                   
+                        <input id="txtTitle" type="text" placeholder="new book title" style={{width:"470px", height:"100px"}} onChange={this.handleNewtitle}/> <br/>
+                        <input type="txtDescription" placeholder="new book description" style={{width:"470px", height:"100px"}} onChange={this.handleNewdesc}/> <br/>
+                        <input type="txtImageUrl" placeholder="new book image-url" style={{width:"470px", height:"100px"}} onChange={this.handleNewimg}/> <br/>                       
+
+                    </form>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.props.handleClose}>Close</Button>
+                    <Button variant="primary" onClick={this.getHandleOpen} >Save changes</Button>
+                </Modal.Footer>
+            </Modal>
+
+            
+        )
+    }
+}
+
+export default BookFormModal
